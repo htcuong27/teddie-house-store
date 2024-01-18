84d8c37af0d798440c599b0a97983d17aee4529f
htcuong27 <cuong.htan27@gmail.com>
2024-01-18 15:51:37 +0700

    fix: remove preline library

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -5,8 +5,7 @@ const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/preline/preline.js',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
@@ -34,7 +33,6 @@ const config: Config = {
     },
   },
   plugins: [
-    require('preline/plugin')
   ],
 };
 export default config;
